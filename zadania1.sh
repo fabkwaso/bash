3ce2639832e0d0a62318bc09e45064c12a07271d
fabkwaso <fabiola.kwasowiec@wp.pl>
2014-03-30 21:23:20 +0200

    Create zadania1.sh

---
+++ zadania1.sh
@@ -0,0 +1,10 @@
+1. cd ~
+2. 
+- ps > plik, ls > plik, who > plik
+- cat > zalogowani CTRL+D
+  who > zalogowani
+  sort zalogowani > zalogowani.alfabetycznie
+  head -n 3 zalogowani.alfabetycznie > zalogowani.head
+4. 
+  who | sort
+  who | sort | head -n 3
