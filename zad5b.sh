58f9dba662514c51ebbb9af7ca346b519569f79e
fabkwaso <fabiola.kwasowiec@wp.pl>
2014-04-04 13:56:25 +0200

    Create zad5b.sh

---
+++ zad5b.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+for ((i = 2;i <= $#;i++)) od drugiego parametru do konca liczby parametrow
+do
+if [ $i = 2 ]; jesli drugi parametr to
+then
+ cp $1 $2 kopiuj plik o nazwie rownej parametrowi nr 1 i nazwij go jako parametr 2   itd itp
+elif [ $i = 3 ];
+then
+ cp $1 $3
+elif [ $i = 4 ];
+then
+ cp $1 $4
+elif [ $i = 5 ];
+then
+ cp $1 $5
+elif [ $i = 6 ];
+then
+ cp $1 $6
+elif [ $i = 7 ];
+then
+ cp $1 $7
+elif [ $i = 8 ];
+then
+ cp $1 $8
+elif [ $i = 9 ];
+then
+ cp $1 $9 
+fi 
+
+done
